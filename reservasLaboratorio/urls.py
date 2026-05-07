13c90ce932d455b554d13ee4757d9e6c76626ebb
Juan Jose <jjperezbravo123@gmail.com>
2026-05-07 13:27:43 -0500

    Update login template path in urls.py

--- reservasLaboratorio/urls.py
+++ reservasLaboratorio/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', auth_views.LoginView.as_view(template_name='appsebastian/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='App/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', include('appsebastian.urls')),
+    path('', include('App.urls')),
 ]
